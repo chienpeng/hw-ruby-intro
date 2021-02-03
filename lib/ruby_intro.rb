fc64c44144587adb895ac1b53d6d1253f19f7e09
Peng <allenhuang818@gmail.com>
2021-02-03 11:11:18 -0600

    first two questions completed

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,28 +4,71 @@
 
 def sum arr
   # YOUR CODE HERE
+  sum = 0
+  if arr.length > 0
+    arr.each do |item|
+      sum += item
+    end
+  end
+  return sum
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr.max
+  else
+    sum = arr.sort[-2] + arr.max
+  return sum
+  end
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  arr.each do |item|
+    remains = n - item
+    arr = arr - [item]
+    arr.each do |item|
+      if remains == item
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  if s[0] =~ /\W/ || s.empty?
+    return false
+  else
+    if s[0] =~ /['A', 'E', 'I' ,'O', 'U', 'a', 'e', 'i', 'o', 'u']/
+      return false
+    else
+      return true
+    end
+  end
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s =~ /\D/ || s.empty?
+    return false
+  end
+  if s =~ /[0-1]/ && s.to_i(2)%4 == 0
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
